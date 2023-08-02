1d7f084a43d4b2242a0bc2626741d7ff021ab259
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-02 23:38:22 +0900

    feat: main view model 구현

---
+++ lib/ui/main/main_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_16day/data/respository/photo_repository.dart';
+import 'package:image_search_16day/ui/main/main_state.dart';
+
+class MainViewModel with ChangeNotifier {
+  final PhotoRepository _photoRepository;
+
+  MainViewModel(this._photoRepository);
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  void fetchImages(String query) async {
+    _state = state.copyWith(
+      isLoading: true,
+    );
+    notifyListeners();
+
+    _state = state.copyWith(
+      photos: await _photoRepository.getPhotos(query),
+      isLoading: false,
+    );
+    notifyListeners();
+  }
+}
